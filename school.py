13c1f77be6b1938ff9d9dc5f7fe17846c739378c
Flatiron School <flatironschool@Flatirons-MacBook-Air.local>
2019-05-20 14:01:27 -0400

    lab progress

--- school.py
+++ school.py
@@ -1,1 +1,26 @@
-class School:
+class School():
+	
+	def __init__(self, name, roster = {}):
+		self.name = name
+		self.roster = roster
+
+	def add_student(self, name, grade):
+		if grade not in list(self.roster.keys()):
+			self.roster[grade] = name
+		else:
+			self.roster[grade] = [self.roster[grade], name]
+
+	def grade(self, grade):
+		return self.roster[grade]
+
+	def sort_roster(self):
+		for key, val in school.roster.items():
+			return (key, ":", school.roster[key])
+			
+
+
+
+
+
+
+
